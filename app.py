a973c974c329956ac5debbbec72ef8c17c29fd65
„Finntrigo“ <Finnwalberlin@live.de>
2023-01-25 11:48:56 +0100

    Kleine Anpassung, TableBooking funktion

--- app.py
+++ app.py
@@ -3,41 +3,76 @@ from flask import render_template, redirect, url_for, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import UserMixin
+from db import User, Reservation, Restaurant, Table
+from werkzeug.security import check_password_hash, generate_password_hash
+import smtplib
 
 #kreiert Flask Instance
 app = Flask(__name__)
 app.secret_key = 'key123'
 #fügt eine database hinzu (https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{DB_Name}'
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #initiiert die database
 db = SQLAlchemy(app)
 DB_Name = "database.db"
-#
+
+
+
 db.init_app(app)
 def __init__(self, name, email):
         self.name = name
         self.email = email
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+###########
+#Tablebooking ist die Reservier-Methode
 
 @app.route("/tablebooking")
 def tablebooking():
     return render_template("tablebooking.html")
 
+@app.route('/tablebooking', methods=['GET', 'POST'])
+def tablebooking():
+    if 'email' in session:
+        if request.method == 'POST':
+            customer_id = User.query.filter_by(email=session['email']).first()
+            restaurant_id = request.form['restaurant']
+            table_count = request.form['table_count']
+            booking_time = request.form['booking_time']
+            booking = Booking(customer_id=customer_id, restaurant_id=restaurant_id, table_count=table_count, booking_time=booking_time)
+            db.session.add(booking)
+            db.session.commit()
+            restaurant_email = User.query.filter_by(id=restaurant_id).first().email
+            send_email(restaurant_email, 'New Booking Request', 'You have a new booking request.')
+            return redirect('/')
+        restaurants = User.query.filter_by(user_type='restaurant').all()
+        return render_template('tablebooking.html', restaurants=restaurants)
+    return redirect('/login')
 
+def send_email(to, subject, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('your_email@gmail.com', 'your_password')
+    msg = f"Subject: {subject}\n\n{message}"
+    server.sendmail('your_email@gmail.com', to, msg)
+    server.quit()
 
-
-
-
+##########
 
 
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
@@ -61,9 +96,15 @@ def signup():
             flash('Password must be at least 7 characters.', category='error')
             pass
         else:
-            #user wird zur database hinzugefügt
+            #user wird kreiert und zur database hinzugefügt
+            #die function "generate_password_hash" encrypted das vom user eingegebene Passwort 
+            #"sha256" ist ein Hasing Algorhythmus, der empfolen wird.
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created.', category='success')
-            
+            return redirect(url_for('home'))
+
     return render_template("signup.html")
 
 @app.route("/login", methods=["POST", "GET"])
--- db.py
+++ db.py
@@ -1,9 +1,25 @@
-from . import db
+from .app import db
 from flask import Flask 
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from os import path
+from werkzeug.security import check_password_hash, generate_password_hash
 
 #database models
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True) #id wird automatisch bei database-eintrag erstellt
+    name = db.Column(db.String(50), nullable=False) #nullable besagt, dass das Feld ausgefüllt werden muss
+    email = db.Column(db.String(120), nullable=False, unique=True) #unique, damit es jede Email nur einmal geben wird
+    password_hash = db.Column(db.String(100)) #verändert, das vom User eingegebene Passwort 
+    role = db.Column(db.String(20)) #entscheidet, ob es ein restaurant ist oder restaurantbesucher
+    date_added = db.Column(db.DateTime, default=datetime.utcnow) #Return the current UTC date and time, with tzinfo None. --> https://docs.python.org/3/library/datetime.html
+    reservations = db.relationship("Reservation", backref="user", lazy=True)
+
+    #wenn der User sich einloggt wird überprüft, ob das Passwort valid ist 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Reservation(db.Model):
     id = db.Column(db.Integer, primary_key=True) #id wird automatisch bei database-eintrag erstellt
     reservationname = db.Column(db.String(50), nullable=False) #nullable besagt, dass das Feld ausgefüllt werden muss
@@ -11,15 +27,6 @@ class Reservation(db.Model):
     reservationdate_added = db.Column(db.DateTime, default=datetime.utcnow) #Return the current UTC date and time, with tzinfo None. --> https://docs.python.org/3/library/datetime.html
     user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
     table_id = db.Column(db.Integer, db.ForeignKey("table.id"), nullable=False)
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True) #id wird automatisch bei database-eintrag erstellt
-    name = db.Column(db.String(50), nullable=False) #nullable besagt, dass das Feld ausgefüllt werden muss
-    email = db.Column(db.String(120), nullable=False, unique=True) #unique, damit es jede Email nur einmal geben wird
-    password_hash = db.Column(db.String(100))
-    role = db.Column(db.String(20)) #entscheidet, ob es ein restaurant ist oder restaurantbesucher
-    date_added = db.Column(db.DateTime, default=datetime.utcnow) #Return the current UTC date and time, with tzinfo None. --> https://docs.python.org/3/library/datetime.html
-    reservations = db.relationship("Reservation", backref="user", lazy=True)
 
 class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,6 +37,7 @@ class Restaurant(db.Model):
 class Table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.Integer)
+    peoplecount = db.Column(db.Integer)
     restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurant.id"), nullable=False)
     reservations = db.relationship("Reservation", backref="table", lazy=True)
 
